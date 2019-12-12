282f3dbc56fed7c8b7ea5d58651f36951d17864f
Justis <lamanna.justis@gmail.com>
2019-12-11 23:36:47 -0600

    Moving server logic into node

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -6,46 +6,54 @@
 import * as path from 'path';
 import { workspace, ExtensionContext } from 'vscode';
 
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+  } from 'vscode-languageclient';
+
 export function activate(context: ExtensionContext) {
-	// // The server is implemented in node
-	// let serverModule = context.asAbsolutePath(
-	// 	path.join('server', 'out', 'server.js')
-	// );
-	// console.log(serverModule);
-	// // The debug options for the server
-	// // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	// let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+	// The server is implemented in node
+	let serverModule = context.asAbsolutePath(
+		path.join('server', 'out', 'server.js')
+	);
+	console.log(serverModule);
+	// The debug options for the server
+	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
-	// // If the extension is launched in debug mode then the debug server options are used
-	// // Otherwise the run options are used
-	// let serverOptions: ServerOptions = {
-	// 	run: { module: serverModule, transport: TransportKind.ipc },
-	// 	debug: {
-	// 		module: serverModule,
-	// 		transport: TransportKind.ipc,
-	// 		options: debugOptions
-	// 	}
-	// };
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: {
+		  module: serverModule,
+		  transport: TransportKind.ipc,
+		  options: debugOptions
+		}
+	  };
 
-	// // Options to control the language client
-	// let clientOptions: LanguageClientOptions = {
-	// 	// Register the server for plain text documents
-	// 	documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-	// 	synchronize: {
-	// 		// Notify the server about file changes to '.clientrc files contained in the workspace
-	// 		fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-	// 	}
-	// };
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: ['xse'],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
 
-	// // Create the language client and start the client.
-	// client = new LanguageClient(
-	// 	'languageServerExample',
-	// 	'Language Server Example',
-	// 	serverOptions,
-	// 	clientOptions
-	// );
+	// Create the language client and start the client.
+	let client = new LanguageClient(
+		'xseLanguageServer',
+		'XSE Language Server',
+		serverOptions,
+		clientOptions
+	);
 
-	// // Start the client. This will also launch the server
-	// client.start();
-	console.log("Hello, world! owo");
+	// Start the client. This will also launch the server
+	let disposable = client.start();
+	
+	context.subscriptions.push(disposable);
 }
